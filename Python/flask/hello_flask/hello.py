7f5dd0c3aa39aebb816f939b6139ea0590863d33
Ian H Petersen <ian.h.petersen@gmail.com>
2021-04-09 20:41:32 -0500

    add html table assignment

--- Python/flask/hello_flask/hello.py
+++ Python/flask/hello_flask/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
@@ -13,6 +13,16 @@ def success():
 def hello(name):
     print(name)
     return f"Hello!  You must be {name}."
+
+@app.route('/lists')
+def render_lists():
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30},
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27},
+        ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
