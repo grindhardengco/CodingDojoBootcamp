6b20c9c87a3f1944a3ea9afd36a271ee13bbeaac
Ian Petersen <80700401+ianhpetersen@users.noreply.github.com>
2021-04-13 16:26:52 -0500

    Update views.py
    remove unnecessary comments.

--- Python/django/time_display/time_app/views.py
+++ Python/django/time_display/time_app/views.py
@@ -7,11 +7,6 @@ def index(request):
         "date": strftime("%b %d, %Y", gmtime()),
         "time": strftime("%H:%M %p", gmtime())
     }
-    # months = [1,'Jan',2,'Feb',3,'Mar',4,'Apr',5,'May',6,'Jun',7,'Jul',8,'Aug',9,'Sep',10,'Oct',11,'Nov',12,'Dec']
-    # month = ''
-    # for i in range (0,len(months)):
-    #     if months[i] == int(strftime("%m", gmtime())):
-    #         month = months[i+1]
     return render (request, 'index.html', context)
 
 def redirect_page(request):
