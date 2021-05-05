bd25d5861363f2bf22e60567d5e0470138c37efb
Ian H Petersen <ian.h.petersen@gmail.com>
2021-05-05 15:14:43 -0500

    update JS Hoisting assignment

--- MEAN/js_hoisting.js
+++ MEAN/js_hoisting.js
@@ -58,13 +58,30 @@ rewind();
 console.log(genre);
 
 //7
+function learn() {
+	var dojo;
+    dojo = "seattle";
+	console.log(dojo);
+	dojo = "burbank";
+	console.log(dojo);
+}
 dojo = "san jose";
 console.log(dojo);
 learn();
-function learn() {
-	dojo = "seattle";
-	console.log(dojo);
-	var dojo = "burbank";
-	console.log(dojo);
-}
 console.log(dojo);
+
+//8
+function makeDojo(name, students){
+        const dojo = {};
+        dojo.name = name;
+        dojo.students = students;
+        if(dojo.students > 50){
+            dojo.hiring = true;
+        }
+        else if(dojo.students <= 0){
+            dojo = "closed for now";
+        }
+        return dojo;
+}
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
