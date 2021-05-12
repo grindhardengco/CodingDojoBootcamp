e020c12225108ef6c49a1abe90abe6119ef0fc14
Ian H Petersen <ian.h.petersen@gmail.com>
2021-05-11 19:43:46 -0500

    update mongo template

--- MEAN/templateProject/server.js
+++ MEAN/templateProject/server.js
@@ -1,23 +1,21 @@
 const express = require("express");
-var path = require("path");
 const session = require("express-session");
 const app = express();
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(session({secret:'whatisIanupto?'}))
-app.use(express.static(path.join(__dirname, "./static")));
+app.use(express.static(__dirname + "/static"));
 mongoose.connect('mongodb://localhost/whatev_name_of_db',{useNewUrlParser:true});
 //change the name of the db & Schema!!!
 const WhatevSchema = new mongoose.Schema({
     whatevkey1: String,
     whatevkey2: Number,
     created_at: {type: Date, default: Date.now},
-})
+})    
 const Whatev = mongoose.model('Whatev',WhatevSchema)
 
-app.set('views', path.join(__dirname, './views'));
+app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 
 // root route to render
@@ -60,7 +58,6 @@ app.post('/arouteforprocessingdata', function(req,res){
 
 // route for rendering db data
 app.get('/thatssomeroute', function(req, res) {
-    whatev = []
     Quote.find()
         .then(function (whatevdata){
             whatev = whatevdata;
@@ -70,11 +67,39 @@ app.get('/thatssomeroute', function(req, res) {
                 key2:whatev.blegh2,
                 key3:whatev.created_at,
         })
+        //or .then(function (whatevdata){res.render('thatejsfile',{whatev:whatevdata[0]})
         .catch(function (err){
             console.log(err);
         })
     });
 })
+
+//render edit 
+app.get('/whatev/edit/:id', function(req, res) {
+    Whatev.find({_id:req.params.id})
+        .then(function(whatevdata){
+            res.render('edit',{whatev:whatevdata[0]});
+        })
+        .catch(function(err){
+            console.log('****errors: ',err)
+        })
+})
+
+//process edit
+app.post('/whatev/:id', function(req,res){
+    //update document from post 
+    Whatev.updateOne({_id:req.params.id},{$set:{
+        whatevkey1: req.body.whatev1,
+        whatevkey2: req.body.whatev2,
+    }})
+        .then(function(whatevdata){
+            res.redirect(`/whatev/${req.params.id}`);
+        })
+        .catch(function(err){
+            console.log('****errors: ',err)
+        })
+})
+
 
 
 app.listen(8000, function() {
