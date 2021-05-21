5450f971e10395f0639c54c12204e8dcedae427c
Ian H Petersen <ian.h.petersen@gmail.com>
2021-05-21 11:18:55 -0500

    update assignment with a nested component

--- MEAN/restful_api_angular/restfulApi/src/app/app.module.ts
+++ MEAN/restful_api_angular/restfulApi/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
+import { DetailComponent } from './detail/detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
