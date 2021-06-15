379320f20ff9d55ec3e80b4f90f2cdb14a9b0c2b
Ian H Petersen <ian.h.petersen@gmail.com>
2021-06-14 21:17:38 -0500

    Add arrays assignment

---
+++ Ruby/arrays.rb
@@ -0,0 +1,21 @@
+a = ["Batman", "got", "on", "my", "nerves."]
+b = ["Superman beat him up."]
+
+# puts a.at(3)
+
+# a.delete("Batman")
+# puts a
+
+# puts a.reverse
+
+# puts a.length
+
+# puts a.sort
+
+# puts a.slice(0,3)
+
+# puts a.shuffle
+
+# puts a.join(" ")
+
+# puts a.insert(5, b)
